e1d23e476ca620290fa4d44d6bb94ff80f170807
aarthib2 <92760173+aarthib2@users.noreply.github.com>
2022-12-11 12:27:56 -0600

    Delete CalcScore.h

--- src/CalcScore.h
+++
@@ -1,9 +0,0 @@
-#include "BFS.h"
-#include "Graph.h"
-#include "csv.h"
-
-#include <vector>
-
-void CalcPrefScore(std::vector<std::vector<int>>, std::vector<int>);
-void CalcAttribScore(std::vector<std::vector<int>>, std::vector<int>);
-std::vector<std::vector<bool>> compat;
