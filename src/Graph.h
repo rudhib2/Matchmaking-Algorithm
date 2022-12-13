949f7dd162c2d296a1a5de61282a1fc3c5f158aa
aarthib2 <92760173+aarthib2@users.noreply.github.com>
2022-12-12 22:47:39 -0600

    Update Graph.h

--- src/Graph.h
+++ src/Graph.h
@@ -1,32 +1,12 @@
 
-// using namespace std;
-// #include iostream
-// #include <vector>
-// #include <list>
+#pragma once 
+
 #include <iostream>
 #include <vector>
 #include <list>
-#pragma once 
 
 using namespace std; 
  
-// // This class represents a directed graph using
-// // adjacency list representation
-// class Graph
-// {
-//     int V;// No. of vertices
- 
-//     // Pointer to an array containing adjacency lists
-//     std::vector<std::list<int>> adj;  
-// public:
-//     Graph(int V);  // Constructor
- 
-//     // function to add an edge to graph
-//     void addEdge(int v, int w); 
-  
-// };
-// This class represents a directed graph using
-// adjacency list representation
 class Graph
 {
 public:
