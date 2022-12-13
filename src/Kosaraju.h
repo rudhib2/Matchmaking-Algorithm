af13adc9c2acda120e014d4154063d469e070c1b
aarthib2 <92760173+aarthib2@users.noreply.github.com>
2022-12-12 19:48:21 -0600

    Update Kosaraju.h

--- src/Kosaraju.h
+++ src/Kosaraju.h
@@ -1,6 +1,10 @@
-using namespace std;
 #include <vector>
 #include <list>
+#include <stack>
+ 
 
-
-std::vector<std::vector<int>> adjacencyMatrix(const std::vector<std::vector<int>> &courses); 
+std::vector<std::vector<int>> adjacency(std::vector<int> attrib_scores, std::vector<int> pref_scores);
+std::vector<std::vector<int>> revadjacency(std::vector<int> attrib_scores, std::vector<int> pref_scores);
+void dfs_1(int x); 
+void dfs_2(int x); 
+int Kosaraju(int n);
