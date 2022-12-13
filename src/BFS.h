c90efd4775773107fd9a10d225e22f85e5e25773
aarthib2 <92760173+aarthib2@users.noreply.github.com>
2022-12-12 22:46:28 -0600

    Update BFS.h

--- src/BFS.h
+++ src/BFS.h
@@ -1,22 +1,4 @@
 
-// #pragma once
-// #include <vector>
-// using std::vector;
-
-// class BFS::Graph {
-//     public:
-//      BFS(); 
-//      public vector<bool> isCompatible(int id1, int id2); // returns yes or no if the score of the ids are within the range
-//      public vector<vector<bool>> populate(vector<int> id); // returns a vector of booleans which denote compatibility
-//      BFS(int id1); 
-//      bool PrelimCheck(int id1, int id2);
-
-//       // part 2 :
-//       //public map<int, vector<int>> compList (vector<vector<bool>>); 
-//       // gives a map which maps the id to the id of people that they are compatible with 
-
-// }
-
 #pragma once
 #include <vector>
 using namespace std; 
@@ -26,10 +8,9 @@ class Traversal{
      bool isCompatible(int id1, int id2); // returns yes or no if the score of the ids are within the range
      std::vector<std::vector<bool>> populate(); // returns a vector of booleans which denote compatibility
      bool PrelimCheck(int id1, int id2);
-     void store_values(); 
+     Traversal(); 
      std::vector<std::vector<int>> adjacency_matrix();
      std::vector<int> compat_list();
-
     std::vector<int> id; 
     std::vector<int> pref_score; // contains the preference score of each person correspoding to id 
     std::vector<int> attr_score; // contains the attribute score of each person correspoding to id 
@@ -37,4 +18,3 @@ class Traversal{
     std::vector<std::vector<int>> attr;
     std::vector<std::vector<int>> adj_matrix; 
 };
-
